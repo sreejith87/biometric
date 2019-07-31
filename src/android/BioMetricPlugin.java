5e03b0a38b5223db04e40aeceed4506e99871667
sreejith87 <sreejith.darmatek@gmail.com>
2019-08-01 00:02:04 +0800

    twelth commit

--- src/android/BioMetricPlugin.java
+++ src/android/BioMetricPlugin.java
@@ -18,10 +18,11 @@ public class BioMetricPlugin extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
-        } else if (action.equals("addMethod")) {
-            this.addMethod(args, callbackContext);
-            return true;
-        }
+        } 
+        // else if (action.equals("addMethod")) {
+        //     this.addMethod(args, callbackContext);
+        //     return true;
+        // }
 
         return false;
     }
@@ -34,18 +35,18 @@ public class BioMetricPlugin extends CordovaPlugin {
         }
     }
 
-    private void addMethod(JSONArray args, CallbackContext callbackContext) {
-        try{
-        String No1 = args.getString(0);
-        //String No2 = args.getString();
-        callbackContext.success("value : " + No1);
-        // int val1 = Integer.parseInt(No1);
-        // int val2 = Integer.parseInt(No2);
-        // int val3 = val1 + val2;
-        // callbackContext.success(val3.toString());
-        } catch(Exception exc) {
-            callbackContext.error(exc.toString());
-        }
+    // private void addMethod(JSONObject args, CallbackContext callbackContext) {
+    //     try{
+    //     String No1 = args.getString(0);
+    //     //String No2 = args.getString();
+    //     callbackContext.success("value : " + No1);
+    //     // int val1 = Integer.parseInt(No1);
+    //     // int val2 = Integer.parseInt(No2);
+    //     // int val3 = val1 + val2;
+    //     // callbackContext.success(val3.toString());
+    //     } catch(Exception exc) {
+    //         callbackContext.error(exc.toString());
+    //     }
         
-    }
+    // }
 }
--- www/BioMetricPlugin.js
+++ www/BioMetricPlugin.js
@@ -1,12 +1,12 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+coolMethod = function (arg0, success, error) {
     exec(success, error, 'BioMetricPlugin', 'coolMethod', [arg0]);
 };
 
-exports.addMethod = function (arg0, success, error) {
-    exec(success, error, 'BioMetricPlugin', 'addMethod', [arg0]);
-};
+// exports.addMethod = function (arg0, success, error) {
+//     exec(success, error, 'BioMetricPlugin', 'addMethod', [arg0]);
+// };
 
 // exports.anotherClassMethod = function (arg0, success, error) {
 //     exec(success, error, 'BioMetricPlugin', 'anotherClassMethod', [arg0]);
