cd98f656e601bb976ff8fc86a86db5d4530076e8
sreejith87 <sreejith.darmatek@gmail.com>
2019-07-31 21:44:35 +0800

    twelth commit

--- src/android/BioMetricPlugin.java
+++ src/android/BioMetricPlugin.java
@@ -36,13 +36,13 @@ public class BioMetricPlugin extends CordovaPlugin {
 
     private void addMethod(JSONArray args, CallbackContext callbackContext) {
         try{
-        String No1 = args.getJSONObject(0).getString("Parm1");
-        String No2 = args.getJSONObject(0).getString("Parm2");
+        String No1 = args.getString("Parm1");
+        String No2 = args.getString("Parm2");
         callbackContext.success(No1 + " " + No2);
-        //int val1 = Integer.parseInt(No1);
-        //int val2 = Integer.parseInt(No2);
-        //int val3 = val1 + val2;
-        //callbackContext.success(val3);
+        // int val1 = Integer.parseInt(No1);
+        // int val2 = Integer.parseInt(No2);
+        // int val3 = val1 + val2;
+        // callbackContext.success(val3.toString());
         } catch(Exception exc) {
             callbackContext.error(exc.toString());
         }
