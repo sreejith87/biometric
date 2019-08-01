c3d9786c5715b2091882c36da08d303ef10d75ed
sreejith87 <sreejith.darmatek@gmail.com>
2019-08-01 15:32:22 +0800

    twelth commit

--- src/android/BioMetricPlugin.java
+++ src/android/BioMetricPlugin.java
@@ -14,42 +14,14 @@ public class BioMetricPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            //String message = args.getString(0);
-            //String message = "sreejith";
-            //this.coolMethod(message, callbackContext);
-            this.coolMethod(callbackContext);
+        if (action.equals("testMethod")) {
+            this.testMethod(callbackContext);
             return true;
         } 
-        // else if (action.equals("addMethod")) {
-        //     this.addMethod(args, callbackContext);
-        //     return true;
-        // }
-
         return false;
     }
 
-    private void coolMethod(CallbackContext callbackContext) {
-        // if (message != null && message.length() > 0) {
-        //     callbackContext.success(message);
-        // } else {
-        //     callbackContext.error("Expected one non-empty string argument.");
-        // }
-        callbackContext.success("Hi sree");
+    private void testMethod(CallbackContext callbackContext) {
+        callbackContext.success("its working");
     }
-
-    // private void addMethod(JSONObject args, CallbackContext callbackContext) {
-    //     try{
-    //     String No1 = args.getString(0);
-    //     //String No2 = args.getString();
-    //     callbackContext.success("value : " + No1);
-    //     // int val1 = Integer.parseInt(No1);
-    //     // int val2 = Integer.parseInt(No2);
-    //     // int val3 = val1 + val2;
-    //     // callbackContext.success(val3.toString());
-    //     } catch(Exception exc) {
-    //         callbackContext.error(exc.toString());
-    //     }
-        
-    // }
 }
--- www/BioMetricPlugin.js
+++ www/BioMetricPlugin.js
@@ -1,13 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'BioMetricPlugin', 'coolMethod', [arg0]);
+exports.testMethod = function (arg0, success, error) {
+    exec(success, error, 'BioMetricPlugin', 'testMethod', [arg0]);
 };
-
-// exports.addMethod = function (arg0, success, error) {
-//     exec(success, error, 'BioMetricPlugin', 'addMethod', [arg0]);
-// };
-
-// exports.anotherClassMethod = function (arg0, success, error) {
-//     exec(success, error, 'BioMetricPlugin', 'anotherClassMethod', [arg0]);
-// };
